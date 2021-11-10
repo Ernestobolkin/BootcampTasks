e322c6c73bb73b73ce3a9b5645f11a4090738d81
Ernestobolkin <ernest1047@gmail.com>
2021-11-10 07:56:30 +0200

    Complete
       

---
+++ js/6.5.js
@@ -0,0 +1,34 @@
+//1
+let arr1 = new Array(100).fill({'test':'test-1'});
+// console.log(arr1);
+
+//2
+let arr2 = Array.from(Array(100).keys());
+// console.log(arr1)
+
+//3
+let obj1 = {a:1,b:2,c:3,d:4};
+let arr3 =Object.values(obj1)
+// console.log(arr3);
+
+//4
+let names = ['Ali','Mike','john','Ben'];
+let obj2 = Object.assign({},names)
+// console.log(obj2)
+
+//5
+let arr4=[1,2,3,4];
+let test={a:1};
+// console,console.log(Array.isArray(arr4)); //true
+// console,console.log(Array.isArray(test)); //false
+
+//6
+let arr5=[1,2,3,4];
+let test2= arr5.slice();
+// console.log(test2)
+
+
+let arr6=[1,2,3,4,5];
+let neWarr6= arr6;
+neWarr6.splice(0,3);
+console.log(neWarr6,arr6)
