ce301af2fbdaf4902fce06ccacf0c8004abab40f
Ernestobolkin <ernest1047@gmail.com>
2021-11-14 17:15:08 +0200

    Ninja

--- js/9.4_for-loop.js
+++ js/9.4_for-loop.js
@@ -5,20 +5,23 @@
 //   let newstr = "#".repeat(i+1)+space
 //   console.log(newstr);
 //   }
-// } 
+// }
 
 // console.log(steps(4))
 
 // Ninja. same thing but witout repeat
 const steps = (n) => {
-  let space= '';
-for(let i = 0; i<n; i++){
-  space += " ";
+  let space = "";
+  for (let i = 0; i < n; i++) {
+    space += "_";
   }
+  // console.log(space);
+  for (let j = 0; j < n; j++) {
+    space = space.replace("_", "#", j);
+    console.log(space);
+  }
+};
 
-for(let i = 0;i<space.length; i++){
-  let newstr = "#".repeat(i+1)+space
-  console.log(newstr);
-  }
-} 
-console.log(steps(4))
+steps(6);
+
+//   let newstr = "#".repeat(i+1)+space
