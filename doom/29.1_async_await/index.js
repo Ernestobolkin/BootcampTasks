6d93ce471e1d82bb795255a20194ee695e062cb3
Ernestobolkin <ernest1047@gmail.com>
2021-12-07 21:10:39 +0200

    done
    all done for roday :)

--- doom/29.1_async_await/index.js
+++ doom/29.1_async_await/index.js
@@ -1,8 +1,8 @@
 const getIDs = () =>
   new Promise((resolve, reject) => {
-    if(true){
-      reject("Code:404")
-    }else{
+    if (!true) {
+      reject("somthing went wrong");
+    } else {
       setTimeout(() => {
         resolve([532, 543, 753, 1, 5]);
       }, 1500);
@@ -26,13 +26,30 @@ const getRecipe = (recipeID) => {
 };
 
 const test = async () => {
-  try{
+  try {
     const ids = await getIDs();
-    const recipe = await getRecipe(ids[2])
-    console.log(recipe);
-  }catch(error){
-    console.log(error,"there is some problem");
+    const rec = await getRecipe(ids[2]);
+    console.log(rec);
+  } catch (error) {
+    console.log(error);
   }
 };
-test()
+test();
 
+// getIDs()
+//   .then((IDs) => {
+//     console.log(IDs);
+//     return getRecipe(IDs[2]);
+//   })
+//   .then((recipe) => {
+//     console.log(recipe);
+//   })
+//   .catch((error) => {
+//     console.log("It is an error!");
+//   });
+// const test = async () => {
+//     const ids = await getIDs();
+//     const recipe = await getRecipe(ids[2])
+//     console.log(recipe);
+
+// };
--- doom/29.2_refactorWithAsyncAwait/index.js
+++ doom/29.2_refactorWithAsyncAwait/index.js
@@ -4,13 +4,12 @@ const h = document.querySelector("h2");
 let theJoke = "";
 
 btn.addEventListener("click", async () => {
-  try{
-    const response = await fetch("https://api.jokasdes.one/jod");
-    const data = await response.json()
-    console.log(data);
-    h.textContent = data.contents.jokes[0].joke.title
+  try {
+    const response = await fetch("https://api.jokes.one/jod");
+    const data = await response.json();
+    h.textContent = data.contents.jokes[0].joke.title;
     p.textContent = data.contents.jokes[0].joke.text;
-  }catch(error){
+  } catch (error) {
     p.textContent = `somthing went wrong, please try agaun later, the error :${error}`;
     console.log(error);
   }
